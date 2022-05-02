ce724b93794518ad954e20ca9d31e7042d77ad60
Elio Bteich <eliobteich115@gmail.com>
2022-05-02 15:36:48 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,101 @@
+let register = null
+let operatorRegister = null
+let operatorElementSelected = null
+let isNextNum = false
+
+$(document).ready(function() {
+    reset()
+    $("#ac").click(function(){
+        reset()
+    })
+    $(".digit").click(function(e){
+        selectDigit(getDigit(e))
+    })
+    $(".operator").click(function(e){
+        setOperator(e)
+    })
+    $(".equal").click(function(){
+        if (operatorRegister == "+") {
+            register += getDisplay()
+        }else if (operatorRegister == "-") {
+            register -= getDisplay()
+        }else if (operatorRegister == "x") {
+            register *= getDisplay()
+        }else if (operatorRegister == "/") {
+            register /= getDisplay()
+        }
+        display(register)
+    })
+})
+
+function display(n) {
+    $(".calc-display-content:nth-child(1)").html(n)
+}
+
+function reset() {
+    display(0)
+    register = null
+    unselectOperator()
+    operatorElementSelected = null
+    operatorRegister = null
+}
+
+function getDisplay() {
+    return parseInt($(".calc-display-content:nth-child(1)").html())
+}
+
+function selectDigit(n) {
+    let currentNum
+    if (!isNextNum){
+        currentNum = getDisplay()
+        currentNum *= 10
+        currentNum += n
+    }else {
+        display(n)
+        isNextNum = false
+    }
+    display(currentNum)
+    unselectOperator()
+}
+
+function getOperator(e) {
+    return String(e.currentTarget.lastElementChild.textContent)
+}
+
+function getDigit(e) {
+    return parseInt(e.currentTarget.lastElementChild.textContent)
+}
+
+function setOperator(e) {
+    if (register == null) {
+        register = getDisplay()
+    }else if (register != null && operatorElementSelected == null){
+        if (operatorRegister == "+") {
+            register += getDisplay()
+        }else if (operatorRegister == "-") {
+            register -= getDisplay()
+        }else if (operatorRegister == "x") {
+            register *= getDisplay()
+        }else if (operatorRegister == "/") {
+            register /= getDisplay()
+        }
+        display(register)
+    }
+    operatorRegister = getOperator(e)
+    unselectOperator()
+    element = e.currentTarget.lastElementChild.parentElement
+    selectOperator(element)
+    isNextNum = true
+}
+
+function selectOperator(element) {
+    $(element).css("background", "white")
+    $(element).css("color", "#ff9500")
+    operatorElementSelected = element
+}
+
+function unselectOperator() {
+    $(operatorElementSelected).css("background", "#ff9500")
+    $(operatorElementSelected).css("color", "white")
+    operatorElementSelected = null
+}
